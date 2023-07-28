b1ed57f3ef046a920633659660943cfa2baf5bda
ansman58 <javanslem@gmail.com>
2023-07-28 19:02:23 +0100

    fix up

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -8,7 +8,6 @@ import ProductThumbnail4 from "../../assets/image-product-4-thumbnail.jpg";
 import Product2 from "../../assets/image-product-2.jpg";
 import Product3 from "../../assets/image-product-3.jpg";
 import Product4 from "../../assets/image-product-4.jpg";
-import { SlideShowContext } from "../../store/contexts";
 import { useWindowSize } from "../../hooks/useWindwSize";
 
 interface ProductsProps {
@@ -24,7 +23,6 @@ const Product: React.FC<ProductsProps> = ({
   showNav,
   setClose,
 }) => {
-  const { showSlides, setShowSlides } = React.useContext(SlideShowContext);
   const [index, setIndex] = React.useState(0);
   const [active, setActive] = React.useState<boolean | number>(false);
   const [currentImage, setCurrentImage] = React.useState(Product1);
@@ -40,7 +38,6 @@ const Product: React.FC<ProductsProps> = ({
   const handleSlides = (indx: number, imgSrc: string) => {
     setClose?.(false);
     setCurrentImage(imgSrc);
-    setShowSlides(true);
     setActive(indx);
   };
 
@@ -113,10 +110,10 @@ const Product: React.FC<ProductsProps> = ({
             alt="product thumnail"
             className={clsx(
               {
-                ["z-100"]: showSlides,
-                ["border-2 border-[white]"]: active === index,
+                ["filter opacity-[40%] border-primaryOrange border-2"]:
+                  active === index,
               },
-              "max-w-[70px] max-h-[70px] rounded-[5px] hover:opacity-[0.67] hover:border-2 border-[#ca611c] cursor-pointer"
+              "max-w-[70px] max-h-[70px] rounded-[5px] hover:filter hover:opacity-[50%] cursor-pointer"
             )}
             onClick={() => handleSlides(index, el.product)}
           />
